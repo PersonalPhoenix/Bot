ec428d3d3c57fee0fc15771a7770ad782237b9a4
PersonalPhoenix <76667360+PersonalPhoenix@users.noreply.github.com>
2022-12-08 15:59:17 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -29,19 +29,33 @@ news_dict = {}
 dp = Dispatcher (bot)
 
 
-#Клава / Keyboard
-news_key = KeyboardButton('/news') #Создаем кнопку / Create button
-news_last = KeyboardButton('/last') #Создаем кнопку / Create button
-cats = KeyboardButton("/kitty 🦊") #Создаем кнопку / Create button
-ti = KeyboardButton ("/TI11") #Создаем кнопку / Create button
-mainMenu = ReplyKeyboardMarkup(resize_keyboard=True).row(news_key,news_last,ti,cats) #Выводи клавиатуру с кнопками / Output keyboard with buttons
+#Back
+backMain = KeyboardButton('/backMain ❌')
+backJuk = KeyboardButton('/backJuk ❌')
+backKitty = KeyboardButton('/bacKitty ❌')
+
+#MainMenu
+niws = KeyboardButton('/news ✉️')
+ixtr = KeyboardButton('/juk 🙂')
+mainMenu = ReplyKeyboardMarkup (resize_keyboard=True).row(niws,ixtr)
 
 
-#Подклава для китиков / Sub keyboard for kitty
-cats_animation = KeyboardButton('/flex_kitty 🐈') #Создаем кнопку / Create button
-cats_picture = KeyboardButton('/tomcat 😼') #Создаем кнопку / Create button
-back = KeyboardButton('/back ❌') #Создаем кнопку / Create button
-catsMenu = ReplyKeyboardMarkup (resize_keyboard=True).row(back,cats_animation,cats_picture) #Возвращаемся из под клавы в глав клаву / Return main keyboard from sub keyborad
+#ixtrMenu
+kits = KeyboardButton('/kitty 🦊')
+joke = KeyboardButton('/joke 😂')
+ixtrMenu = ReplyKeyboardMarkup(resize_keyboard=True).row(kits,joke,backMain)
+
+
+#KitsMenu
+sticker = KeyboardButton('/sticker 🎆') 
+image = KeyboardButton('/image 🖼')
+kitsMenu = ReplyKeyboardMarkup(resize_keyboard=True).row(sticker,image,backJuk)
+
+
+#StickerMenu
+static = KeyboardButton('/tomcat 😼')
+flexkit = KeyboardButton('/flexkitty 🐈')
+stickerMenu = ReplyKeyboardMarkup(resize_keyboard=True).row(static,flexkit,backKitty)
 
 
 #Сообщение в CMD о старте / Message about start in CMD
@@ -53,11 +67,12 @@ async def on_startup (_): #Задаем функцию / Setting the function
 @dp.message_handler (commands = ["start"]) #Задаем комманду / Setting the command
 async def start  (message: types.Message): #Задаем функцию / Setting the function
     await bot.send_message( message.from_user.id, 'YEP начинаем',reply_markup = mainMenu) #Отправляем приветствие / Send hello
+    await bot.send_message( message.from_user.id, 'Если хочешь узнать что я умею, пиши /help')
     await bot.send_sticker(message.chat.id,"CAACAgIAAxkBAAEFiWli9TsP9B1BLa8HrEqL51EtH4UQEAACnhYAArpIyUtcjAeK6Rs_SykE") #Отправляем стикер / Send sticker
-
+    
 
 #Парсер с выводом / Parse with output
-@dp.message_handler (commands=["news"]) 
+@dp.message_handler (commands=["NewsDota"]) 
 async def news (message: types.Message):
     header = {
     'user-agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.167 YaBrowser/22.7.5.940 Yowser/2.5 Safari/537.36"
@@ -112,10 +127,10 @@ async def news (message: types.Message):
             news = f"<b>{v['time']}</b>\n"\
             f"{hlink(v['title'],v['url'])}"
             await message.answer(news)
-    
+
 
 #Cписок id стикеров статичных котов / List id stickers static cats
-kit = ["CAACAgIAAxkBAAEGReBjYN1bncaxE0k4O8yqWpHidwq-QQADFwACBud5SAZAQLkOAjyhKgQ", #CATS
+stat = ["CAACAgIAAxkBAAEGReBjYN1bncaxE0k4O8yqWpHidwq-QQADFwACBud5SAZAQLkOAjyhKgQ", #CATS
 "CAACAgIAAxkBAAEF9I9jNd9ItkiG4oztiyly1n3Z0jD8SAACIwADezwGEa6cmphaatyTKgQ",
 "CAACAgIAAxkBAAEGReZjYN23Pp7-EJ9uxQbGg1r0mLThsAACRhAAAnN6CUt9kgUt0XU89ioE",
 "CAACAgIAAxkBAAEGRehjYN3B63RU0TqbkSlFW0vdW4ZPfAACjg8AApdNCUsYXNqjioE8WioE",
@@ -135,21 +150,94 @@ flex = ["CAACAgIAAxkBAAEFkWti-oEXVf1fIkJDQjvMjDt2WyAZ1wACNBcAAkOaoEhNfT8fIoHPmCk
 "CAACAgIAAxkBAAEGRd5jYN1Z98oRRs3rbbIAAYoN6juyUdwAAk4YAAJPp9BL5qYnFJhB_RwqBA",
 "CAACAgIAAxkBAAEGReRjYN2UUuk4qPEw7A7EwR0_Sf_WWwACKhcAAu_jyEvWhp-EtkEyvioE"]
 
-#Клавиатуры киттиков / Kitty keyboard
-@dp.message_handler()
-async def kitty (message):       
-    if message.text == "/kitty 🦊":
-        await bot.send_message( message.from_user.id, 'Выбери какого котика ты хочешь',reply_markup = catsMenu)
-    if message.text == "/flex_kitty 🐈":
-        await bot.send_sticker(message.chat.id,choice(flex))
-    elif message.text == "/tomcat 😼":
-        await bot.send_sticker(message.chat.id,choice(kit))
-    elif message.text == '/back ❌':
-        await bot.send_message(message.from_user.id,"Вернемся к делу",reply_markup = mainMenu)
-    elif message.text == "/TI11":
-        await bot.send_message(message.from_user.id,"""The Intenational 11 проходил в Сингпуре. 
-Дата проведения 15.10.2022 - 30.10.2022. 
-Победителем по результатам игр стал европейский коллектив Tundra Esports""")
+#список разрывных
+jokers = ["""Заходит однажды в бар улитка и говорит:
+-Можно виски с колой?
+- Простите, но мы не обслуживаем улиток.
+И бармен вышвырнул ее за дверь.
+Через неделю заходит опять эта улитка и спрашивает:
+-Ну и нахуя ты это сделал!?""",
+"""Бабушка перешла дорогу не на тот свет,
+и попала на тот свет.""",
+"""Как называют человека, который продал свою печень?
+Обеспеченный."""]
+
+
+@dp.message_handler (commands=["help"])
+async def help (message: types.Message):
+    await bot.send_message(message.chat.id, """Я специализируюсь на новостях киберспорта и критовалют, но также умею выдавать прикольчики
+    Для новостей напиши /news
+    Для прикольчиков напиши /juk
+Также можно воспользоваться клавиатурой если писать впадлу.
+У клавиатуры есть более быстрая навигация, чтобы ее узнать напиши /more""")
+
+@dp.message_handler (commands=["more"])
+async def more (message: types.Message):
+    await bot.send_message(message.chat.id,"""Для быстрого получения контента можно прописать команду, а не прокликивать по клаве.
+Полный список комманд:
+    /start (начать общение)
+    /help (помощь)
+    /more (больше информации)
+    /back (вернуться в предыдущее меню)
+    /juk (вывести меню приколов)
+    /news (вывести меню новостей)
+    /joke (получить разрывную)
+    /sticker (вывод меню стикеров с котами)
+    /tomcat (получить статичный стикер с котом)
+    /flexkitty (получить динамичный стикер с котом)
+    /image (получить картинку кота)""")
+
+@dp.message_handler (commands=["juk"])
+async def prikl(message: types.Message):
+    await bot.send_message(message.from_user.id, "Выбери прикольчик",reply_markup=ixtrMenu)
+
+@dp.message_handler (commands=["joke"])
+async def jk (message: types.Message):
+    await bot.send_message(message.chat.id, choice(jokers))
+
+@dp.message_handler (commands=["kitty"])
+async def k (message: types.Message):
+    await bot.send_message(message.from_user.id,"Выбери формат котика",reply_markup=kitsMenu)
+
+@dp.message_handler (commands=["sticker"])
+async def stic (message: types.Message):
+    await bot.send_message(message.from_user.id,"Выбери формат стикера",reply_markup=stickerMenu)
+
+@dp.message_handler (commands=["tomcat"])
+async def sta (message: types.Message):
+    await bot.send_sticker(message.chat.id,choice(stat))
+
+@dp.message_handler (commands=["flexkitty"])
+async def flx (message: types.Message):
+    await bot.send_sticker(message.chat.id,choice(flex))
+
+
+#фразы для BACK
+literaly = ["Даем себаса","Оформляем себастьяна","Давим себастьяна","Базируемся в прошлое","Go back in time","Удаляем будущее","Шерудим себа","Обертас","Взадпятки"]
+
+
+#лист с фото
+im = ["https://commons.wikimedia.org/wiki/File:Cat%27s_Eyes.jpg?uselang=ru","https://commons.wikimedia.org/wiki/File:Black_Footed_Cat.jpg?uselang=ru"]
+
+
+@dp.message_handler (commands=["image"])
+async def img (message: types.Message):
+    await bot.send_photo(message.chat.id,choice(im))
+
+#BackИ   
+
+@dp.message_handler (commands=["backMain"])
+async def backmain (message: types.Message):
+    await bot.send_message(message.from_user.id,choice(literaly),reply_markup=mainMenu)
+
+@dp.message_handler (commands=["backJuk"])
+async def backjuk (message: types.Message):
+    await bot.send_message(message.from_user.id,choice(literaly),reply_markup=ixtrMenu)
+
+@dp.message_handler (commands=["bacKitty"])
+async def backitty (message: types.Message):
+    await bot.send_message(message.from_user.id,choice(literaly),reply_markup=kitsMenu)
+
 
 #запуск бота  
 if __name__ == "__main__":
